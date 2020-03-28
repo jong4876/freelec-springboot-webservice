7b827d4d367f8fcd6639b1d597d1f67b564b51bd
jong4876 <jong4876@users.noreply.github.com>
2020-03-28 23:18:15 +0900

    fix : h2 enable 설정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,6 +2,10 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.annotation.WebServlet;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
